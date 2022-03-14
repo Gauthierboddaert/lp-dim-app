ab52b0748bd44c185b5d0bb6d0b24c7f018aaa3f
Gauthier Boddaert <boddaert.gauthier@gmail.com>
2022-03-14 20:43:11 +0100

    AngularProject

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [FooterComponent, HeaderComponent],
+})
+export class CoreModule {}
